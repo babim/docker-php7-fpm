2c613a6b26f1f4b80eeea5d5026725c9e8b02c6c
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-07 02:20:14 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,24 @@
-#!/bin/bash
-if [ -z "`ls /etc/php/7.0`" ] 
+#!/bin/sh
+if [ -z "`ls /etc/php7`" ] 
 then
-	cp -R /etc-start/php/7.0/* /etc/php/7.0
+	cp -R /etc-start/php7/* /etc/php7
 fi
 
-#service php7.0-fpm start
-bash
+    # Set environments
+    TIMEZONE1=${TIMEZONE:-Asia/Ho_Chi_Minh}
+    PHP_MEMORY_LIMIT1=${PHP_MEMORY_LIMIT:-512M}
+    MAX_UPLOAD1=${MAX_UPLOAD:-520M}
+    PHP_MAX_FILE_UPLOAD1=${PHP_MAX_FILE_UPLOAD:-200}
+    PHP_MAX_POST1=${PHP_MAX_POST:-520M}
+    MAX_INPUT_TIME1=${MAX_INPUT_TIME:-3600}
+    MAX_EXECUTION_TIME1=${MAX_EXECUTION_TIME:-3600}
+	
+	sed -i "s|;*date.timezone =.*|date.timezone = ${TIMEZONE}|i" /etc/php7/php.ini && \
+	sed -i "s|;*memory_limit =.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" /etc/php7/php.ini && \
+ 	sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${MAX_UPLOAD}|i" /etc/php7/php.ini && \
+    	sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|i" /etc/php7/php.ini && \
+    	sed -i "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST}|i" /etc/php7/php.ini && \
+    	sed -i "s/max_input_time = 60/max_input_time = 3600/" /etc/php7/php.ini && \
+	sed -i "s/max_execution_time = 30/max_execution_time = 3600/" /etc/php7/php.ini
+	
+exec "$@"
