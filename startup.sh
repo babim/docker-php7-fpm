36f4cfacc6c6d0bd4abfdd433d3e59a43be989ca
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-09 09:21:39 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -14,15 +14,17 @@ fi
     MAX_INPUT_TIME1=${MAX_INPUT_TIME:-3600}
     MAX_EXECUTION_TIME1=${MAX_EXECUTION_TIME:-3600}
 	
-	sed -i "s|;*date.timezone =.*|date.timezone = $TIMEZONE|i" /etc/php7/php.ini && \
-	sed -i "s|;*memory_limit =.*|memory_limit = $PHP_MEMORY_LIMIT|i" /etc/php7/php.ini && \
- 	sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = $MAX_UPLOAD|i" /etc/php7/php.ini && \
-    	sed -i "s|;*max_file_uploads =.*|max_file_uploads = $PHP_MAX_FILE_UPLOAD|i" /etc/php7/php.ini && \
-    	sed -i "s|;*post_max_size =.*|post_max_size = $PHP_MAX_POST|i" /etc/php7/php.ini && \
-    	sed -i "s/max_input_time = 60/max_input_time = $MAX_INPUT_TIME/" /etc/php7/php.ini && \
-	sed -i "s/max_execution_time = 30/max_execution_time = $MAX_EXECUTION_TIME/" /etc/php7/php.ini
+	sed -i -E \
+	-e "s|;*date.timezone =.*|date.timezone = ${TIMEZONE}|i" \
+	-e "s|;*memory_limit =.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" \
+ 	-e "s|;*upload_max_filesize =.*|upload_max_filesize = ${MAX_UPLOAD}|i" \
+    	-e "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|i" \
+    	-e "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST}|i" \
+    	-e "s/max_input_time = 60/max_input_time = ${MAX_INPUT_TIME}/" \
+	-e "s/max_execution_time = 30/max_execution_time = ${MAX_EXECUTION_TIME}/" \
+	-e "s/;opcache.enable=0/opcache.enable=0/" \
+	/etc/php7/php.ini
 
-    sed -i "s/;opcache.enable=0/opcache.enable=0/" /etc/php7/php.ini && \
     sed -i -e "s/;daemonize\s*=\s*yes/daemonize = no/g" /etc/php7/php-fpm.conf && \
     sed -i '/^listen = /clisten = 9000' /etc/php5/php-fpm.d/www.conf && \
     sed -i '/^listen.allowed_clients/c;listen.allowed_clients =' /etc/php7/php-fpm.d/www.conf && \
