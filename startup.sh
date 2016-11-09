839b60430957bb5c309ef8a01321e8f60581192b
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-09 21:31:15 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-if [ -z "`ls /etc/php/7.0`" ] 
-then
-	cp -R /etc-start/php/7.0/* /etc/php/7.0
-fi
+if [ -z "`ls /etc/php/7.0`" ]; then cp -R /etc-start/php/5.6/* /etc/php/5.6; fi
+
+   # Set environments
+    TIMEZONE1=${TIMEZONE:-Asia/Ho_Chi_Minh}
+    PHP_MEMORY_LIMIT1=${PHP_MEMORY_LIMIT:-512M}
+    MAX_UPLOAD1=${MAX_UPLOAD:-520M}
+    PHP_MAX_FILE_UPLOAD1=${PHP_MAX_FILE_UPLOAD:-200}
+    PHP_MAX_POST1=${PHP_MAX_POST:-520M}
+    MAX_INPUT_TIME1=${MAX_INPUT_TIME:-3600}
+    MAX_EXECUTION_TIME1=${MAX_EXECUTION_TIME:-3600}
+	
+	sed -i -E \
+	-e "s|;*date.timezone =.*|date.timezone = ${TIMEZONE1}|i" \
+	-e "s|;*memory_limit =.*|memory_limit = ${MAX_UPLOAD1}|i" \
+ 	-e "s|;*upload_max_filesize =.*|upload_max_filesize = ${MAX_UPLOAD1}|i" \
+    	-e "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD1}|i" \
+    	-e "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST1}|i" \
+    	-e "s/max_input_time = 60/max_input_time = ${MAX_INPUT_TIME1}/" \
+	-e "s/max_execution_time = 30/max_execution_time = ${MAX_EXECUTION_TIME1}/" \
+	/etc/php/7.0/*/php.ini
+
+    sed -i '/^listen = /clisten = 9000' /etc/php/7.0/fpm/pool.d/www.conf && \
+    sed -i '/^listen.allowed_clients/c;listen.allowed_clients =' /etc/php/7.0/fpm/pool.d/www.conf && \
+    sed -i '/^;catch_workers_output/ccatch_workers_output = yes' /etc/php/7.0/fpm/pool.d/www.conf && \
+    sed -i '/^;env\[TEMP\] = .*/aenv[DB_PORT_3306_TCP_ADDR] = $DB_PORT_3306_TCP_ADDR' /etc/php/7.0/fpm/pool.d/www.conf
 
