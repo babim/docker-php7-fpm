b22d0a62d85f3d968213cb7e346a60188f86396b
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-11 13:22:31 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -45,6 +45,8 @@ elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
 	export auser=root
 	sed -i -e "/^user = .*/cuser = $auser" /etc/php/7.0/fpm/php-fpm.conf
 	sed -i -e "/^group = .*/cgroup = $auser" /etc/php/7.0/fpm/php-fpm.conf
+elif id $auid >/dev/null 2>&1; then
+        echo "UID exists. Please change UID"
 else
 if id $auser >/dev/null 2>&1; then
         echo "user exists"
