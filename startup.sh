25c3f9d1dd283340b1addce16aa806a030cd48a0
Duc Anh Babim <ducanh.babim@yahoo.com>
2017-12-31 01:22:08 +0700

    1

--- startup.sh
+++ startup.sh
@@ -20,6 +20,8 @@ if [ -z "`ls /etc/php`" ]; then
     	-e "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST1}|i" \
     	-e "s/max_input_time = 60/max_input_time = ${MAX_INPUT_TIME1}/" \
 	-e "s/max_execution_time = 30/max_execution_time = ${MAX_EXECUTION_TIME1}/" \
+	-e "s/error_reporting = .*/error_reporting = E_ALL/" \
+	-e "s/display_errors = .*/display_errors = On/" \
 	/etc/php/7.0/*/php.ini
 	sed -i -e "s|^;*\(opcache.enable\) *=.*|\1 = 1|" /etc/php/7.0/*/php.ini
         sed -i -e "s|^;*\(opcache.enable_cli\) *=.*|\1 = 1|" /etc/php/7.0/*/php.ini
