f3e549087de04144570226ab01d703005d76703c
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-06-29 03:09:21 +0700

    1

--- startup.sh
+++ startup.sh
@@ -74,11 +74,7 @@ fi
 
 fi
 
-# ssh
-if [ -f "/runssh.sh" ]; then /runssh.sh; fi
-# cron
-if [ -f "/runcron.sh" ]; then /runcron.sh; fi
-# nfs
-if [ -f "/mountnfs.sh" ]; then /mountnfs.sh; fi
+# option with entrypoint
+if [ -f "/option.sh" ]; then /option.sh; fi
 
 php-fpm7.1 -F
